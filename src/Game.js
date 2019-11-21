eb454207b7af645e4250d8b70f296c92d46295f7
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-11-21 10:55:32 +0100

    Preventing score reuse

--- src/Game.js
+++ src/Game.js
@@ -83,7 +83,7 @@ class Game extends Component {
               <button
                 className="Game-reroll"
                 disabled={
-                  this.state.locked.every(x => x) || this.state.rollsLeft == 0
+                  this.state.locked.every(x => x) || this.state.rollsLeft === 0
                 }
                 onClick={this.roll}
               >
